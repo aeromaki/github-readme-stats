ae862d34da57b5e6787bbf05dbe58f8d99061f61
-3σ <yurippe@m3sigma.net>
2023-08-16 03:56:11 +0900

    commented out unused endpoints

--- express.js
+++ express.js
@@ -1,7 +1,7 @@
-import statsCard from "./api/index.js";
-import repoCard from "./api/pin.js";
+// import statsCard from "./api/index.js";
+// import repoCard from "./api/pin.js";
 import langCard from "./api/top-langs.js";
-import wakatimeCard from "./api/wakatime.js";
+// import wakatimeCard from "./api/wakatime.js";
 import express from "express";
 import dotenv from "dotenv";
 
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.listen(process.env.port || 9000);
 
-app.get("/", statsCard);
-app.get("/pin", repoCard);
+// app.get("/", statsCard);
+// app.get("/pin", repoCard);
 app.get("/top-langs", langCard);
-app.get("/wakatime", wakatimeCard);
+// app.get("/wakatime", wakatimeCard);
